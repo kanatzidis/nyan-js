5902e727dd456641ee8f8d35e07ad3c343a5ce7b
Gregori Kanatzidis <kanatzidisg@gmail.com>
2018-04-01 19:05:43 -0500

    only do this once

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
 var frames = require('./frames');
 var characters = require('./characters');
 
+var initialized = false;
+
 function nyan(options = {}) {
   var {
     colors,
     pure,
     stream } = options;
 
-  frames.forEach(function(e, i) {
-    frames[i] = e.slice(17, 50);
-    if(colors) {
-      frames[i] = frames[i].map(function(row) {
-        return row.split('').map(function(c) {
-          // I don't think any characters are unaccounted for but you never know
-          if(characters.draw[c]) {
-            return characters.draw[c] + (pure ? ' ' : c) + characters.end;
-          } else {
-            return c;
-          }
-        }).join('');
-      });
-    }
-    frames[i] = frames[i].join('\n');
-  });
+  if(!initialized) {
+    frames.forEach(function(e, i) {
+      frames[i] = e.slice(17, 50);
+      if(colors) {
+        console.log(typeof frames[i]);
+        frames[i] = frames[i].map(function(row) {
+          return row.split('').map(function(c) {
+            // I don't think any characters are unaccounted for but you never know
+            if(characters.draw[c]) {
+              return characters.draw[c] + (pure ? ' ' : c) + characters.end;
+            } else {
+              return c;
+            }
+          }).join('');
+        });
+      }
+      frames[i] = frames[i].join('\n');
+    });
+  }
+
+  initialized = true;
   
   var i = 0;
   return setInterval(function() {
